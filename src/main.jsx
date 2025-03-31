bf50be94be83d3ffdc631b4654a1cf5f6ef6c2d2
kotlalokeshwari <kotlalokeshwari1871@gmail.com>
2025-03-31 12:03:49 +0530

    commiting the project

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from '../src/pages/Home'
+import Vans from '../src/pages/Vans'
+
+
+function Header() {
+  return (
+    <>
+      <BrowserRouter>
+        <header>
+
+            <Link className='site-logo' to='/'>#VANLIFE</Link>
+          <nav>
+
+            <Link to='/vans'>Vans</Link>
+
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path='/' element={<Home />}>Home</Route>
+          <Route path='/vans' element={<Vans />}>vans</Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+createRoot(document.getElementById('root')).render(
+
+  <Header />
+
+)
